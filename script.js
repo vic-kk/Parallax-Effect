7687e6d5c4990b4a0c243d7f0a527aa385ca07b0
Victor K <dev@victorkarasev.ru>
2023-04-03 10:52:51 +0700

    refactor code

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 parallaxBox = document.getElementById('parallaxBox'),
 layers = parallaxBox.getElementsByClassName('layer');
 
-parallaxBox.onmousemove = (event) => {
-  const xPos = (window.innerWidth/2 - event.clientX - parallaxBox.offsetLeft)/10;
-  const yPos = (window.innerHeight/2 - event.clientY - parallaxBox.offsetTop)/10;
-  
+const getNewPositions = (event, moveAsCursor = false) => {
+  const { clientX, clientY } = event;
+  const calcAxis = (axis) => (window.innerWidth / 2 - axis - parallaxBox.offsetLeft) / 10;
+  const newPositions = {
+    x: calcAxis(clientX),
+    y: calcAxis(clientY),
+  }
+  if (moveAsCursor) return {
+    x: -newPositions.x,
+    y: -newPositions.y,
+  }
+  return newPositions;
+}
+
+const updateMovement = (event) => {
+  const positions = getNewPositions(event, true);
   for (var i = 0; i < layers.length; i++) {
     const speed = Number(layers[i].dataset.layerSpeed);
-    layers[i].style.webkitTransform = `translate(${-xPos * speed}px, ${-yPos * speed / 2}px)`;
+    layers[i].style.webkitTransform = `translate(${positions.x * speed}px, ${positions.y * speed / 2}px)`;
   }
 }
+
+parallaxBox.onmousemove = updateMovement;
